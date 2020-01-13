28ef273b825d6053e9ee84e068e7f17591be40f4
Adam Greenfield <ajamesgreenfield@gmail.com>
2020-01-13 21:53:42 +0000

    feat(movement): update npc motor

--- Assets/Scenes/scripts/NpcMotor.cs
+++ Assets/Scenes/scripts/NpcMotor.cs
@@ -8,4 +8,23 @@ using UnityEngine.AI;
 public class NpcMotor : MonoBehaviour
 {
 
+    NavMeshAgent agent;
+    public LayerMask movementMask;
+
+
+    public GameObject goal;
+
+    // Start is called before the first frame update
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(goal.transform.position);
+        Debug.Log(transform.position);
+
+        agent.SetDestination(GameObject.Find("Chair").transform.position); 
+    }
 }
