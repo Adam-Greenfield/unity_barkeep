a9255f42315d3c1ea7f13c6ccbe7477ecb22696b
Adam <adam@radical-departures.com>
2020-05-10 18:56:10 +0100

    feat(menu)
    create ui panel to open as a result of interactable openMenu()

--- Assets/Scenes/scripts/Interactable/MenuObject.cs
+++ Assets/Scenes/scripts/Interactable/MenuObject.cs
@@ -5,6 +5,11 @@ public class MenuObject : MonoBehaviour
 {
     public GameObject menu;
     private GameObject instMenu;
+    private Camera camera;
+    private Vector2 canvasPos;
+    private Vector2 screenPoint;
+    private Canvas menuCanvas;
+    private RectTransform rectTransform;
 
     void Start()
     {
@@ -15,14 +20,19 @@ public class MenuObject : MonoBehaviour
     {
         if (instMenu)
         {
-            instMenu.transform.rotation = Camera.main.transform.rotation;
+            //instMenu.transform.rotation = Camera.main.transform.rotation * Quaternion.Euler(270, 0, 0);
         }
     }
 
     public void open(Vector3 parentPosition)
     {
+        menuCanvas = instMenu.GetComponent<Canvas>();
+        rectTransform = menuCanvas.GetComponent<RectTransform>();
+        camera = Camera.main;
+        screenPoint = Camera.main.WorldToScreenPoint(parentPosition);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(menuCanvas., screenPoint, null, out canvasPos);
         Debug.Log("I am the menu, here I open!");
         instMenu = Instantiate(menu);
-        instMenu.transform.position = parentPosition;
+        //instMenu.transform.localPosition = Vector3.MoveTowards(parentPosition, camera.transform.position, 2);
     }
 }
