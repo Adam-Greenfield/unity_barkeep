1e8feaea1c38ffd171ac4b7739af4c5b863841c9
Adam Greenfield <ajamesgreenfield@gmail.com>
2020-05-31 22:15:48 +0100

    feat(Interactables)
    create new interactable classes

---
+++ Assets/Scenes/scripts/Interactable/Item/ItemInteractor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteractor : Interactable
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void InspectFromMenu()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void InteractFromMenu()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+}
