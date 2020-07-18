2a3fd461951868f9ad4b336806fde2b1cc66cc92
Adam Greenfield <ajamesgreenfield@gmail.com>
2020-07-18 22:14:20 +0100

    enable enemy ai to follow player

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = PlayerManager.instance.player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+    public static PlayerManager instance;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.Log("more than one instance of PlayerManager");
+        }
+        instance = this;
+    }
+    #endregion
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
