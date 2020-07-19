fe2cc7dddde984970a0538d71edeeb1fe7e14cae
Adam Greenfield <ajamesgreenfield@gmail.com>
2020-07-19 14:03:18 +0100

    update aminator with coroutine to prevent overwriting attack

---
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCombat : MonoBehaviour
+{
+    bool animationLocked;
+
+    public Animator animator;
+    // Use this for initialization
+    void Start()
+    {
+        animationLocked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!animationLocked)
+                Attack();
+        }
+    }
+
+    void Attack()
+    {
+        StartCoroutine(PlayAnimation("Attack"));
+    }
+
+    IEnumerator PlayAnimation(string trigger)
+    {
+        animationLocked = true;
+
+        animator.SetTrigger("Attack");
+
+        while (animator.GetCurrentAnimatorStateInfo(0).fullPathHash != Animator.StringToHash("Base Layer.Attack_punch"))
+        {
+            yield return null;
+        }
+
+        float counter = 0;
+        float waitTime = animator.GetCurrentAnimatorStateInfo(0).length;
+
+        while (counter < (waitTime))
+        {
+            counter += Time.deltaTime;
+            yield return null;
+        }
+
+        //Done playing. Do something below!
+        Debug.Log("Done Playing");
+
+        animationLocked = false;
+    }
+}
